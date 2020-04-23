9f0f60620f12fb91e590187d167fdd78dc64bd49
Adão Lima <adaoolima@gmail.com>
2020-04-23 01:37:31 -0300

    Added get and delete actions

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,14 +1,69 @@
 import { Router } from 'express';
+import { getRepository, getCustomRepository } from 'typeorm';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
 // import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
+// interface CategoryResponse {
+//   id: string;
+//   title: string;
+//   created_at: Date;
+//   updated_at: Date;
+// }
+// interface TransactionsResponse {
+//   id: string;
+//   title: string;
+//   value: number;
+//   type: 'income' | 'outcome';
+//   category: CategoryResponse;
+//   created_at: Date;
+//   updated_at: Date;
+// }
+
+// interface Transactions {
+//   transactions: Array<TransactionsResponse>;
+//   balance: {
+//     income: number;
+//     outcome: number;
+//     total: number;
+//   };
+// }
+
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+  const categoriesRepository = getRepository(Category);
+  const transactions = await transactionsRepository.find();
+  const transactionsResponse = transactions.map(item => {
+    return {
+      id: item.id,
+      title: item.title,
+      value: item.value,
+      type: item.type,
+      category: categoriesRepository.findOne({
+        where: { id: item.category_id },
+      }),
+      created_at: item.created_at,
+      updated_at: item.updated_at,
+    };
+  });
+  const { income, outcome, total } = await transactionsRepository.getBalance();
+
+  const transactionsFinal = {
+    transactions: transactionsResponse,
+    balance: {
+      income,
+      outcome,
+      total,
+    },
+  };
+
+  return response.json(transactionsFinal);
 });
 
 transactionsRouter.post('/', async (request, response) => {
@@ -16,7 +71,13 @@ transactionsRouter.post('/', async (request, response) => {
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.body;
+
+  const deleteTransaction = new DeleteTransactionService();
+
+  const transaction = await deleteTransaction.execute({ id });
+
+  return response.status(204).json(transaction);
 });
 
 transactionsRouter.post('/import', async (request, response) => {
