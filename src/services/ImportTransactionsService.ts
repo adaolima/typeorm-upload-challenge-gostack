827b0a46c18460eb999c161d05cc0e3f32fc41d5
Adão Lima <adaoolima@gmail.com>
2020-04-24 22:27:57 -0300

    Importing files

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,51 +1,73 @@
-import { getRepository } from 'typeorm';
+import { getRepository, getCustomRepository } from 'typeorm';
 import path from 'path';
 import fs from 'fs';
 import parse from 'csv-parse';
+import 'csv-parse/lib/es5';
 
 import uploadConfig from '../config/upload';
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
-import CreateTransactionService from './CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+// import CreateTransactionService from './CreateTransactionService';
 
 interface Request {
   csvFilename: string;
 }
-
 class ImportTransactionsService {
   async execute({ csvFilename }: Request): Promise<Transaction[]> {
-    const transactionsRepository = getRepository(Transaction);
-    // const transactions = await transactionsRepository(Transaction);
-    const createTransaction = new CreateTransactionService();
-
-    const csvFilePath = path.join(uploadConfig.directory, csvFilename);
-    const loadedCsvDatas = fs.createReadStream(csvFilePath);
+    const transactionRepository = getRepository(Transaction);
+    const transactionFilePath = path.join(uploadConfig.directory, csvFilename);
+    const transacionsData = fs.readFileSync(transactionFilePath);
+    const output: Array<string> = [];
     const parser = parse({ delimiter: ',' });
-    const output: Transaction[] = [];
-    parser.on('data', async data => {
-      const [title, type, value, category] = data;
-      const transaction = await createTransaction.execute({
-        title,
-        value,
-        type,
-        category,
-      });
-      output.push(transaction);
-    });
-    loadedCsvDatas.pipe(parser);
-    output.shift();
 
-    output.map(async item => {
-      const findTransactions = await transactionsRepository.findOne({
-        title: item.title,
-      });
-      if (!findTransactions) {
-        await transactionsRepository.save(item);
+    parser.on('readable', function () {
+      let record;
+      // eslint-disable-next-line no-cond-assign
+      // prettier-ignore
+      while (record = parser.read()) {
+        output.push(record);
+        console.log(record);
       }
     });
+    // Catch any error
+    parser.on('error', function (err) {
+      console.error(err.message);
+    });
 
-    return output;
+    // parser.on('end', function () {
+    //   console.log(output);
+    // });
+
+    console.log(output);
+
+    parser.write(transacionsData);
+
+    parser.end();
+    // prettier-enable
+    // const transactions = transacionsData.pipe(parser);
+
+    // const parser = parse({ delimiter: ',' }, function (err, data) {
+    //   return data;
+    // data.map((item: Transaction, index: number) => {
+
+    //   if (index !== 0) {
+    //     // const { title, type, value, category } = item;
+    //     // const transaction = await transactionRepository.create({
+    //     //   title,
+    //     //   type,
+    //     //   value,
+    //     //   category,
+    //     // });
+    //     // transactionRepository.save(transaction);
+    //     // output.push(item);
+    //   }
+    // });
+    // });
+    // const transactions = transacionsData.pipe(parser);
+
+    // console.log(transactions);
   }
 }
 
